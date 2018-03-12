94fcbf44f218927df333b398fc9859c506ffad1d
Derek Sims <DerekSims@Dereks-MBP-4.fios-router.home>
2018-03-11 20:06:38 -0400

    updated file

--- lab5/log_analysis.py
+++ lab5/log_analysis.py
@@ -1,1 +1,11 @@
-File for log analysis
+# File for log analysis
+
+arr [1, 2, 3, 4]
+
+
+# with open('test.txt','r') as f:
+#     with open('test1.txt','w') as g: 
+#         for x in f:
+#             x = x.rstrip()
+#             if not x: continue
+#             print >> g, int(x, 16)
