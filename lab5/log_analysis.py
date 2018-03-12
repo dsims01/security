8c643d65260da41680070cab47c13619ec570f48
Derek Sims <DerekSims@Dereks-MBP-4.fios-router.home>
2018-03-12 18:11:22 -0400

    updated

--- lab5/log_analysis.py
+++ lab5/log_analysis.py
@@ -1,11 +1,18 @@
 # File for log analysis
+import re
 
-arr [1, 2, 3, 4]
+ips = []
 
 
-# with open('test.txt','r') as f:
+with open('access.log','r') as f:
 #     with open('test1.txt','w') as g: 
-#         for x in f:
+         for x in f:
+         	line = x.rstrip()
+         	found_ip = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})',line)
+         	if found_ip:
+         		print found_ip[0]
+         		ips.append(found_ip[0])
+         	
 #             x = x.rstrip()
 #             if not x: continue
 #             print >> g, int(x, 16)
