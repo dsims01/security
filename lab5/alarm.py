bddda3164fab72c54b60f39025a89f0c476a420d
root <derek.sims@tufts.edu>
2018-03-13 19:20:04 -0400

    update

--- lab5/alarm.py
+++ lab5/alarm.py
@@ -4,12 +4,51 @@ from scapy.all import *
 import pcapy
 import argparse
 
+iNum = 1
+user_no_password = ""
+
+
 def packetcallback(packet):
-  try:
-    if packet[TCP].dport == 80:
-      print "HTTP (web) traffic detected!"
+  global user_no_password
+  try: 
+     payload = ""
+     if packet[Raw] != None:
+        load = str(packet[Raw])
+        start = load.find("Authorization: Basic")
+        if start != -1:
+            end = load.find('\r', start)
+            encoded = load[start+21:end]
+            payload = ' (' + encoded.decode('base64') + ')'
+            printAlarm ("username and password sent in-the-clear", packet[IP].src,"",payload)
+        else:
+            start = load.find("USER")
+            if start != -1:
+                end = load.find('\r',start)
+                user_no_password = load[start+5:end]
+            if user_no_password != "":
+                start = load.find("PASS")
+                if start != -1:
+                    end = load.find('\r', start)
+                    password = load[start+5:end] 
+                    payload = ' (' + user_no_password + ':' + password + ')'
+                    printAlarm("username and password sent in-the-clear", packet[IP].src,"",payload)
+        found_nikto = load.find("nikto")
+        if found_nikto != -1:
+            printAlarm("Nikto scan", packet[IP].src, "HTTP","") 
+
+     if packet[TCP].flags == 1:
+         printAlarm("FIN scan", packet[IP].src,"",payload) 
+     if packet[TCP].flags == 0:
+         printAlarm("NULL scan", packet[IP].src,"",payload)
+     if packet[TCP].flags == 41:
+         printAlarm("XMAS scan", packet[IP].src,"",payload)
   except:
     pass
+
+def printAlarm(incident, ip, protocol, payload):
+    global iNum
+    print 'ALERT #{}: {} is detected from {}{}{}'.format(iNum,incident,ip,protocol,payload)
+    iNum = iNum+1
 
 parser = argparse.ArgumentParser(description='A network sniffer that identifies basic vulnerabilities')
 parser.add_argument('-i', dest='interface', help='Network interface to sniff on', default='eth0')
@@ -28,4 +67,4 @@ else:
   except pcapy.PcapError:
     print "Sorry, error opening network interface %(interface)s. It does not exist." % {"interface" : args.interface}
   except:
-    print "Sorry, can\'t read network traffic. Are you root?"
+    print "Sorry, can't read network traffic. Are you root?"
